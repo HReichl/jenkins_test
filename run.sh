32ed9c516a1f09ee54845992b2c7a2304b4490d4
HReichl <31277938+HReichl@users.noreply.github.com>
2020-04-13 09:57:09 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+hrdate
 echo "This is the script from GitHub." > run.out
 echo -n "Date: " >> run.out
 date >> run.out
 echo "Line added in GitHub UI"
 echo "Line added on Jenkins master server"
-#hrdate
